f09ccbc5e9d9d4e1a774885e82e2217c9ff91b2a
dzavadindev <daniil.zavadin@gmail.com>
2024-02-07 21:07:40 +0100

    added basic movement

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -3,10 +3,13 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(delta) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left","move_right")
+	input.z = Input.get_axis("move_forward","move_back")
+	
+	apply_central_force(input * 1200.0 * delta)
