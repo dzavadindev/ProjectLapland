4cde7bc8ca5b5bde67f57f1269cb8bdc2e8b7e7b
dzavadindev <daniil.zavadin@gmail.com>
2024-02-08 17:24:52 +0100

    added dash to the players movement, attack + enemy WIP

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,10 +1,11 @@
 extends RigidBody3D
 
+var dash_avalible = true;
+var dash_length = 2000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
@@ -12,4 +13,20 @@ func _process(delta) -> void:
 	input.x = Input.get_axis("move_left","move_right")
 	input.z = Input.get_axis("move_forward","move_back")
 	
-	apply_central_force(input * 1200.0 * delta)
+	# Determines if player is able to dash at that momenent
+	if (Input.get_action_strength("dash") && dash_avalible) :
+		dash_avalible = false;
+		$DashCooldown.start();
+		apply_central_impulse(input * dash_length * delta)
+	
+	# Horizontal : recomended boost ratio is 5/7, 
+	# everything else makes diagonal movement wanky 
+	if (input.x != 0 && input.z != 0) :
+		apply_central_force(input * 700.0 * delta)
+	else :
+		apply_central_force(input * 1000.0 * delta)
+	
+
+func _on_dash_cooldown_timeout():
+	dash_avalible = true;
+
